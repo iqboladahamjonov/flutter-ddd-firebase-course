ec125fe3d59ff33a9fb4b84a916c347327d93fd4
IAdakhamjonov <iqbol.adahamjonov@gmail.com>
2021-06-17 18:43:28 +0500

    16 - Watching Notes on Firestore

--- lib/application/notes/note_actor/note_actor_bloc.dart
+++ lib/application/notes/note_actor/note_actor_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_firebase_ddd_course/domain/notes/i_notes_repository.dart';
 import 'package:notes_firebase_ddd_course/domain/notes/note.dart';
@@ -11,6 +12,7 @@ part 'note_actor_event.dart';
 part 'note_actor_state.dart';
 part 'note_actor_bloc.freezed.dart';
 
+@injectable
 class NoteActorBloc extends Bloc<NoteActorEvent, NoteActorState> {
   final INoteRepository _noteRepository;
   NoteActorBloc(this._noteRepository) : super(const NoteActorState.initial());
